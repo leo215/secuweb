93daadf7e3bf7d7b89ee408dfd18825294f837c9
leo215 <willykedio@gmail.com>
2019-07-04 02:23:27 +0200

    Fixture Address + HomeController

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\DataFixtures;
+
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Address;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $address = new Address();
+        $address->setStreet("14 rue de la victoire");
+        $address->setCp("29000");
+        $address->setCity("Cotonou");
+        $address->setCountry("Bénin");
+
+        $manager->persist($address);
+        $this->setReference('address-1', $address);
+
+        $address = new Address();
+        $address->setStreet("1 Rue du Parc au Ray");
+        $address->setCp("44000");
+        $address->setCity("Nantes");
+        $address->setCountry("France");
+
+        $manager->persist($address);
+        $this->setReference('address-2', $address);
+
+
+        $address = new Address();
+        $address->setStreet("12 Rue de victoire");
+        $address->setCp("28999");
+        $address->setCity("Maroc");
+
+        $manager->persist($address);
+        $this->setReference('address-3', $address);
+
+        $address = new Address();
+        $address->setStreet("05 rue clermont");
+        $address->setCp("44470");
+        $address->setCity("THOUARE SUR LOIRE");
+        $address->setCountry("FRANCE");
+
+        $manager->persist($address);
+        $this->setReference('address-4', $address);
+        $manager->flush();
+    }
+}
